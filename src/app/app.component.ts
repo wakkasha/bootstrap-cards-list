beee3a8ae372b9a55f5ce8a2361d2ba88f53684a
Shawn Walker <swalker@concordservicing.com>
2020-10-13 19:43:27 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-app';
+
+  somethings:any[]= [];
+
+  constructor(){
+    this.somethings = [{
+      text: "Test 1",
+      body: "This is the body 1"
+    },
+    {
+      text: "Test 2",
+      body: "This is the body 2"
+    },
+    {
+      text: "Test 3",
+      body: "This is the body 3"
+    },
+    {
+      text: "Test 4",
+      body: "This is the body 4"
+    },
+    {
+      text: "Test 5",
+      body: "This is the body 5"
+    },
+    {
+      text: "Test 6",
+      body: "This is the body 6"
+    },
+
+    {
+      text: "Test 7",
+      body: "This is the body 7"
+    },
+    {
+      text: "Test 8",
+      body: "This is the body 8"
+    },
+    {
+      text: "Test 9",
+      body: "This is the body 9"
+    },
+    {
+      text: "Test 10",
+      body: "This is the body 10"
+    },
+
+
+  ];
+  }
 }
